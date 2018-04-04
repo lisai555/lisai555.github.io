0060728947aac1bc2de4793723f21623deb46f5b
lisai555 <lisai555@163.com>
2018-04-04 16:00:52 +0800

    Site updated: 2018-04-04 16:00:46

--- photo/tool.js
+++ photo/tool.js
@@ -1,26 +1,25 @@
 "use strict";
-    const fs = require("fs");
-    const path = "../../photos";
-
-    fs.readdir(path, function (err, files) {
-        if (err) {
+var fs = require("fs");
+var path = "./photos/";
+fs.readdir(path, function (err, files) {
+    if (err) {
+        return;
+    }
+    var arr = [];
+    (function iterator(index) {
+        if (index == files.length) {
+            fs.writeFile("./source/photos/data.json", JSON.stringify(arr, null, "\t"));
+            console.log('get img success!');
             return;
         }
-        let arr = [];
-        (function iterator(index) {
-            if (index == files.length) {
-                fs.writeFile("output.json", JSON.stringify(arr, null, "\t"));
+        fs.stat(path + files[index], function (err, stats) {
+            if (err) {
                 return;
             }
-
-            fs.stat(path + "/" + files[index], function (err, stats) {
-                if (err) {
-                    return;
-                }
-                if (stats.isFile()) {
-                    arr.push(files[index]);
-                }
-                iterator(index + 1);
-            })
-        }(0));
-    });
+            if (stats.isFile()) {
+                arr.push(files[index]);
+            }
+            iterator(index + 1);
+        })
+    }(0));
+});
